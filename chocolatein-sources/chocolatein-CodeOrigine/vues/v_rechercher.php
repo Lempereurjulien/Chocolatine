1f0ea3b4d0a4513484cf26470231fb0e2db77261
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-04-01 14:54:09 +0200

    php sur la page v_rechercher

---
+++ chocolatein-sources/chocolatein-CodeOrigine/vues/v_rechercher.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html>
+    <head>
+    <h1><center>Rechercher un mot</center></h1>
+    <center><input type="text" id="recherche" class="text_box name"></center><button>Rechercher</button>
+    </head>
+</html>
